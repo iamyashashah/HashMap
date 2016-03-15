fd2898ee431679acc198be780779bdc64632c4b9
iamyashashah <yashshah.1512@gmail.com>
2016-03-15 17:03:25 -0400

    first commit

---
+++ src/HashMaps.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.*;
+
+public class HashMaps {
+	static Scanner sc=new Scanner(System.in);
+	static int number;
+	static String value;
+	static HashMap<Integer, String> myMap = new HashMap<Integer, String>();
+	static File file = new File("test2.txt");
+
+	public static void main(String[] args) {
+
+		Read();
+		System.out.println("Enter a key :");
+		number = sc.nextInt();
+		if(myMap.containsKey(number))
+		{
+			System.out.println(myMap.get(number));
+		}
+		else
+		{
+			System.out.println("Value not present");
+			System.out.println("enter a value for the key:");
+			value=sc.next(); //scanning the value
+			myMap.put(number, value); //Mapping the value
+			//System.out.println(myMap.get(number));//Testing purpose
+		}
+		Write();
+
+	}
+	public static void Read() 
+	{
+		try
+		{
+			Scanner input =new Scanner(file);
+			while(input.hasNextLine())
+			{
+				String line =input.nextLine();
+				String []numval=line.split(" ");
+				int newnumber=Integer.parseInt(numval[0]);
+				myMap.put(newnumber, numval[1]);
+			}
+			input.close();
+
+		}
+		catch(IOException e)
+		{
+			e.getMessage().toString();
+		}
+	}
+
+	
+	public static void Write()
+	{
+		try{
+
+			file.createNewFile();
+			FileWriter writer = new FileWriter(file); 
+			for(int n:myMap.keySet())
+			{
+				writer.write(n +" "+ myMap.get(n)+"\n"); 
+			}
+			writer.flush();
+			writer.close();
+		}
+		catch(IOException e)
+		{
+			e.getMessage().toString();
+		}
+	}
+	
+
+
+}
